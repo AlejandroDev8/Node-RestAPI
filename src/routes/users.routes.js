0f9ea73d4d08819124a28cd3e29a949f1e1858e8
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-04-27 19:10:39 -0600

    Add users routes to users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,8 +2,31 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.send("Hello from users routes");
+});
+
 router.get("/users", (req, res) => {
   res.send("Users");
+});
+
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`User ${id}`);
+});
+
+router.post("/users", (req, res) => {
+  res.send("User created");
+});
+
+router.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`User ${id} updated`);
+});
+
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`User ${id} deleted`);
 });
 
 export default router;
